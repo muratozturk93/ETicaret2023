d253ba7c3a4846e744b66910976a35a31f534633
muratozturk93 <muratozturk93@outlook.com>
2023-01-02 14:52:13 +0300

    Add project files.

---
+++ ETicaret2023/Controllers/KategorilerController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret2023.Models;
+
+namespace ETicaret2023.Controllers
+{
+    public class KategorilerController : Controller
+    {
+        private ETicaretEntities db = new ETicaretEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Kategoriler.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            if (kategoriler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoriler);
+        }
+
+        // GET: Kategoriler/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Kategoriler kategoriler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kategoriler.Add(kategoriler);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kategoriler);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            if (kategoriler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoriler);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Kategoriler kategoriler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kategoriler).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kategoriler);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            if (kategoriler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoriler);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            db.Kategoriler.Remove(kategoriler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ETicaret2023/Controllers/UrunlerController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret2023.Models;
+
+namespace ETicaret2023.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        private ETicaretEntities db = new ETicaretEntities();
+
+        // GET: Urunler
+        public ActionResult Index()
+        {
+            var urunler = db.Urunler.Include(u => u.Kategoriler);
+            return View(urunler.ToList());
+        }
+
+        // GET: Urunler/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = db.Urunler.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(urunler);
+        }
+
+        // GET: Urunler/Create
+        public ActionResult Create()
+        {
+            ViewBag.KategoriID = new SelectList(db.Kategoriler, "KategoriID", "KategoriAdi");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Urunler urunler,HttpPostedFileBase UrunResim)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Urunler.Add(urunler);
+                db.SaveChanges();
+
+                if (UrunResim!=null)
+                {
+                    string dosya = Path.Combine(Server.MapPath("~/Resim"), urunler.UrunID + ".jfif");
+                    UrunResim.SaveAs(dosya);
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.KategoriID = new SelectList(db.Kategoriler, "KategoriID", "KategoriAdi", urunler.KategoriID);
+            return View(urunler);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = db.Urunler.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.KategoriID = new SelectList(db.Kategoriler, "KategoriID", "KategoriAdi", urunler.KategoriID);
+            return View(urunler);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Urunler urunler, HttpPostedFileBase UrunResim)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(urunler).State = EntityState.Modified;
+                db.SaveChanges();
+
+                if (UrunResim != null)
+                {
+                    string dosya = Path.Combine(Server.MapPath("~/Resim"), urunler.UrunID + ".jfif");
+                    UrunResim.SaveAs(dosya);
+                }
+                return RedirectToAction("Index");
+            }
+            ViewBag.KategoriID = new SelectList(db.Kategoriler, "KategoriID", "KategoriAdi", urunler.KategoriID);
+            return View(urunler);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = db.Urunler.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(urunler);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Urunler urunler = db.Urunler.Find(id);
+            db.Urunler.Remove(urunler);
+            db.SaveChanges();
+
+            string dosya = Path.Combine(Server.MapPath("~/Resim"), id + ".jfif");
+
+            FileInfo fi = new FileInfo(dosya);
+            if (fi.Exists)
+            {
+                fi.Delete();
+            }
+
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
